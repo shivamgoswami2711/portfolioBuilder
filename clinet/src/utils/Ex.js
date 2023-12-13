a224f56744f24594c25f04a624b4effa64e42cd6
shivam goswami <shivamgoswami6261282518@gmail.com>
2023-12-13 23:30:21 +0530

    modifyed function

--- clinet/src/utils/Ex.js
+++ clinet/src/utils/Ex.js
@@ -1,22 +1,81 @@
-import React, { useState } from "react";
+function jsonToJSX(
+  jsonData: JSONElement,
+  onDragStart: (e: React.DragEvent<HTMLDivElement>, element: JSONElement) => void,
+  onDragOver: (e: React.DragEvent<HTMLDivElement>, element: JSONElement) => void,
+  onDragLeave: (e: React.DragEvent<HTMLDivElement>) => void,
+  onDrop: (e: React.DragEvent<HTMLDivElement>, element: JSONElement) => void,
+  onTextChange: (text: string, id: string) => void,
+  onDoubleClick: (e: React.MouseEvent<HTMLDivElement>) => void,
+  onBlurEvent: (e: React.FocusEvent<HTMLDivElement>) => void
+): JSX.Element {
+  const {
+    tag,
+    text,
+    customId,
+    class: className,
+    children,
+    childAllow,
+    disableChild,
+    customAttributes,
+    style,
+    selfClosing
+  } = jsonData;
 
-function Ex() {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState(0);
-  return (
-    <div>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <input
-        type="number"
-        value={age}
-        onChange={(e) => setAge(e.target.value)}
-      />
-    </div>
-  );
+  const attributes: {[key: string]: string | Function | Object} = {
+    ...customAttributes,
+    style: {...style}
+  };
+
+  if (className) attributes.className = className;
+  if (customId) attributes.id = customId;
+
+  attributes.draggable = 'true';
+  attributes.onDragStart = (e: React.DragEvent<HTMLDivElement>) => onDragStart(e, jsonData);
+  attributes.onDragOver = (e: React.DragEvent<HTMLDivElement>) => onDragOver(e, jsonData);
+  attributes.onDragLeave = (e: React.DragEvent<HTMLDivElement>) => onDragLeave(e);
+  attributes.onDrop = (e: React.DragEvent<HTMLDivElement>) => onDrop(e, jsonData);
+  attributes.onDoubleClick = (e: React.MouseEvent<HTMLDivElement>) => onDoubleClick(e);
+  attributes.onBlur = (e: React.FocusEvent<HTMLDivElement>) => onBlurEvent(e);
+
+  attributes.key = customId || generateRandomString(3);
+
+  attributes.onInput = (e: React.FormEvent<HTMLElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    onTextChange(e.currentTarget.textContent || "", e.currentTarget.id);
+  };
+
+  attributes.onDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    e.currentTarget.classList.add("dragedElement");
+    onDragOver(e, jsonData);
+  };
+
+  attributes.onDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    e.currentTarget.classList.remove("dragedElement");
+    onDragOver(e, jsonData);
+  };
+
+  if (selfClosing) {
+    return React.createElement("img", {...attributes}, null);
+  }
+
+  if (!children && !text) {
+    return React.createElement(tag, {...attributes});
+  }
+
+  if (disableChild || childAllow && (childAllow.length > 0 && !childAllow.includes(tag))) {
+    // Child is disabled or not allowed
+    return React.createElement(tag, {...attributes}, text);
+  }
+
+  // Recursively process children
+  const childElements = children?.map((child, index) => {
+    return jsonToJSX(child, onDragStart, onDragOver, onDragLeave, onDrop, onTextChange, onDoubleClick, onBlurEvent);
+  });
+
+  return React.createElement(tag, {...attributes}, childElements);
 }
-
-export default Ex;
